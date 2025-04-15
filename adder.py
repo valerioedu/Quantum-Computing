f820cf43073ee2256d5a99c70e3dc66aec4a3096
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-04-15 23:44:12 +0200

    8 bit adder

--- adder.py
+++ adder.py
@@ -1,35 +1,83 @@
-import qiskit_aer
-import qiskit
+from qiskit import QuantumRegister as QuantumRegister
+from qiskit import ClassicalRegister as ClassicalRegister
+from qiskit import QuantumCircuit as QuantumCircuit
+from qiskit_aer import AerSimulator
 
-qr = qiskit.QuantumRegister(7, 'q1')
-cr = qiskit.ClassicalRegister(2, 'c')
-q2 = qiskit.QuantumRegister(1, 'q2')
-qc = qiskit.QuantumCircuit(qr, cr, q2)
+def quantum_full_adder(qc, a, b, cin, sum_, carry, temp1, temp2, temp3):
+    qc.cx(a, sum_)
+    qc.cx(b, sum_)
+    qc.cx(cin, sum_)
 
-qc.x(qr[0])
-qc.x(qr[1])
-qc.x(qr[2])
+    qc.ccx(a, b, temp1)
+    qc.ccx(a, cin, temp2)
+    qc.ccx(b, cin, temp3)
 
-qc.cx(qr[0], qr[3])
-qc.cx(qr[1], qr[3])
-qc.cx(qr[2], qr[3])
+    qc.cx(temp1, carry)
+    qc.cx(temp2, carry)
+    qc.cx(temp3, carry)
 
-qc.ccx(qr[0], qr[1], qr[5])
-qc.ccx(qr[0], qr[2], qr[6])
+n = 8
 
-qc.ccx(qr[1], qr[2], q2[0])
+a = QuantumRegister(n, 'a')
+b = QuantumRegister(n, 'b')
+sum_ = QuantumRegister(n, 'sum')
+carry = QuantumRegister(n + 1, 'carry')
+temp1 = QuantumRegister(n, 't1')
+temp2 = QuantumRegister(n, 't2')
+temp3 = QuantumRegister(n, 't3')
+cr = ClassicalRegister(n + 1, 'c')
 
-qc.cx(qr[5], qr[4])
-qc.cx(qr[6], qr[4])
-qc.cx(q2[0], qr[4])
+qc = QuantumCircuit(a, b, sum_, carry, temp1, temp2, temp3, cr)
 
-qc.measure(qr[3], cr[0])
-qc.measure(qr[4], cr[1])
+bin_a = input('Enter the first number (A) in binary: ')
+bin_b = input('Enter the second number (B) in binary: ')
 
-backend = qiskit_aer.Aer.get_backend('qasm_simulator')
+bin_a = bin_a.strip().replace(" ", "")
+bin_b = bin_b.strip().replace(" ", "")
 
-print("Circuit diagram:")
-print(qc.draw())
+if not all(bit in '01' for bit in bin_a) or not all(bit in '01' for bit in bin_b):
+    raise ValueError("Inputs must contain only binary digits (0 and 1)")
+
+bin_a = bin_a.zfill(n)[-n:]
+bin_b = bin_b.zfill(n)[-n:]
+
+for i in range(n):
+    if bin_a[n-1-i] == '1':
+        qc.x(a[i])
+    if bin_b[n-1-i] == '1':
+        qc.x(b[i])
+
+
+for i in range(n):
+    quantum_full_adder(
+        qc,
+        a=a[i],
+        b=b[i],
+        cin=carry[i],
+        sum_=sum_[i],
+        carry=carry[i+1],
+        temp1=temp1[i],
+        temp2=temp2[i],
+        temp3=temp3[i]
+    )
+
+for i in range(n):
+    qc.measure(sum_[i], cr[i])
+qc.measure(carry[n], cr[n])
+
+backend = AerSimulator(method='matrix_product_state')
+
+for i in range(3):
+    ans = input("Do you want to see the circuit diagram? (y/n): ")
+    if ans.lower() == 'y':
+        print("Circuit diagram:")
+        print(qc.draw())
+        break
+    elif ans.lower() == 'n':
+        print("Circuit diagram not displayed.")
+        break
+    else:
+        print("Invalid input, please enter 'y' or 'n'.")
 
 job = backend.run(qc, shots=1024)
 result = job.result()
